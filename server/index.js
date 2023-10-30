046001bff730dfd0d950eec583f1a4c5e58bc0ae
Shehan Lakshitha <shehanlaksh@gmail.com>
2023-10-30 08:01:57 +0530

    added routes

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import clientRoutes from './routes/client.js';
+import generalRoutes from './routes/general.js';
+import managementRoutes from './routes/management.js';
+import salesRoutes from './routes/sales.js';
 
 
 /* Config */
