07a6841035e59ff4377605c266df54c277fefeec
Shehan Lakshitha <shehanlaksh@gmail.com>
2023-11-15 19:40:58 +0530

    Added material UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
-
+import { CssBaseline,ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "theme";
 
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
-      
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,19 @@ import "./index.css";
 import App from "./App";
 import { configureStore } from "@reduxjs/toolkit";
 import globalReducer from "./state";
+import { Provider } from "react-redux";
+
+const store = configureStore({
+  reducer: {
+    global: globalReducer,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
