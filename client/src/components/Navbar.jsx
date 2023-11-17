f28973b6d456e5f516fa80c33614daba9beac3e6
Shehan Lakshitha <shehanlaksh@gmail.com>
2023-11-17 21:40:52 +0530

    Add sidebar and search functionality to Navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,7 +11,13 @@ import FlexBetween from "components/FlexBetween";
 import { useDispatch } from "react-redux";
 import { setMode } from "state";
 import profileImg from "assets/profile.jpg";
-import { AppBar, Toolbar, useTheme } from "@mui/material";
+import {
+  AppBar,
+  IconButton,
+  InputBase,
+  Toolbar,
+  useTheme,
+} from "@mui/material";
 import { red } from "@mui/material/colors";
 
 const NavBar = () => {
@@ -28,6 +34,36 @@ const NavBar = () => {
     >
       <Toolbar sx={{ justifyContent: "space-between" }}>
         {/* LEFT */}
+        <FlexBetween>
+          <IconButton onClick={() => console.log("open/cose sidebar")}>
+            <MenuIcon />
+          </IconButton>
+          <FlexBetween
+            backgroundColor={theme.palette.background.alt}
+            borderRadius="9px"
+            gap="3rem"
+            p="0.1rem 1.5rem"
+          >
+            <InputBase placeholder="Search..." />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        </FlexBetween>
+
+        {/* RIGHT */}
+        <FlexBetween gap="1.5rem">
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === "dark" ? (
+              <DarkModeOutlined sx={{ fontSize: "25px" }} />
+            ) : (
+              <LightModeOutlined sx={{ fontSize: "25px" }} />
+            )}
+          </IconButton>
+          <IconButton>
+            <SettingsOutlined sx={{ fontSize: "25px" }} />
+          </IconButton>
+        </FlexBetween>
       </Toolbar>
     </AppBar>
   );
--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,16 +1,31 @@
-import React,{ useState } from 'react';
-import { Box, useMediaQuery } from '@mui/material';
-import { Outlet } from 'react-router-dom';
-import { UseSelector } from 'react-redux';
+import React, { useState } from "react";
+import { Box, useMediaQuery } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import { UseSelector } from "react-redux";
 import Navbar from "components/Navbar";
+import Sidebar from "components/Sidebar";
 
 const Layout = () => {
-  return <Box width="100%" height="100%">
-    <Box>
-      <Navbar />
-      <Outlet />
-    </Box>
-  </Box>
-}
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
-export default Layout
+  return (
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <Sidebar
+        isNonMobile={isNonMobile}
+        drawerwidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Box>
+        <Navbar
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
+
+export default Layout;
