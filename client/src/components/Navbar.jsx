40da38520ab67e01afa5f7ee2c8981af832ddf6d
Shehan Lakshitha <shehanlaksh@gmail.com>
2023-11-17 18:58:03 +0530

    Delete image file and update Navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  LightModeOutlined,
+  DarkModeOutlined,
+  Menu as MenuIcon,
+  Search,
+  SettingsOutlined,
+  ArrowDropDownOutlined,
+} from "@mui/icons-material";
+import FlexBetween from "components/FlexBetween";
+import { useDispatch } from "react-redux";
+import { setMode } from "state";
+import profileImg from "assets/profile.jpg";
+import { AppBar, Toolbar, useTheme } from "@mui/material";
+import { red } from "@mui/material/colors";
 
 const NavBar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const dispatch = useDispatch();
+  const theme = useTheme();
 
-export default NavBar
+  return (
+    <AppBar
+      sx={{
+        position: "static",
+        background: "none",
+        boxShadow: "none",
+      }}
+    >
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        {/* LEFT */}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default NavBar;
