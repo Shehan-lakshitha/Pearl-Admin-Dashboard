94642a90f4a013cc13ae9da2aacaf611b1bc19ac
Shehan Lakshitha <shehanlaksh@gmail.com>
2023-12-03 20:50:28 +0530

    Fix import paths and add initial data

--- server/controllers/general.js
+++ server/controllers/general.js
@@ -1,4 +1,4 @@
-import User from "../models/User";
+import User from "../models/User.js";
 
 export const getUser = async (req, res) => {
   try {
--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
+//data imports
+import User from "./models/User.js";
+import { dataUser } from "./data/index.js";
+
 /* Config */
 dotenv.config();
 const app = express();
@@ -29,9 +33,15 @@ app.use("/sales", salesRoutes);
 
 /* DB Connection */
 const PORT = process.env.PORT || 5000;
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
-}).catch((error) => console.log(`${error} did not connect`));
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+    /* Add data one time */
+    User.insertMany(dataUser);
+    
+  })
+  .catch((error) => console.log(`${error} did not connect`));
