34824831035af97696c4bbaaf171b128827e8506
Shehan Lakshitha <shehanlaksh@gmail.com>
2023-12-29 20:34:35 +0530

    Refactor transactions component to fetch and display transaction data

--- client/src/scenes/transactions/index.jsx
+++ client/src/scenes/transactions/index.jsx
@@ -1,17 +1,57 @@
-import React, { useState} from 'react'
-import { DataGrid } from '@mui/x-data-grid'
-import { useGetTransactionsQuery } from 'state/api'
-import Header from 'components/Header'
-import { useTheme } from '@mui/material/styles';
-
+import React, { useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { useGetTransactionsQuery } from "state/api";
+import Header from "components/Header";
+import { Box, useTheme } from "@mui/material";
 
 const Transactions = () => {
-    const theme = useTheme();
-  return (
-    <div>
-      Transactions
-    </div>
-  )
-}
+  const theme = useTheme();
 
-export default Transactions
+  //values to be sent to the backend
+  const [page, setPage] = useState(0);
+  const [pageSize, setPageSize] = useState(20);
+  const [sort, setSort] = useState({});
+  const [search, setSearch] = useState("");
+
+  const { data, isLoading } = useGetTransactionsQuery({
+    page,
+    pageSize,
+    sort: JSON.stringify(sort),
+    search,
+  });
+
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+    },
+    {
+      field: "userId",
+      headerName: "User ID",
+      flex: 1,
+    },
+    {
+      field: "createdAt",
+      headerName: "Created At",
+      flex: 1,
+    },
+    {
+      field: "products",
+      headerName: "No. of Products",
+      flex: 0.5,
+      sortable: false,
+      renderCell: (params) => params.value.length,
+    },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      renderCell: (params) => `$${Number(params.value).toFixed(2)}`,
+    },
+  ];
+
+  return <div>Transactions</div>;
+};
+
+export default Transactions;
