a45259f0e1566982310c0ff33a60625d250921c5
Shehan Lakshitha <shehanlaksh@gmail.com>
2023-12-30 20:35:42 +0530

    Add custom toolbar to Transactions data grid

---
+++ client/src/components/DataGridCustomToolbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Search } from "@mui/icons-material";
+import { IconButton, TextField, InputAdornment } from "@mui/material";
+import {
+  GridToolbarDensitySelector,
+  GridToolbarContainer,
+  GridToolbarExport,
+  GridToolbarColumnsButton,
+} from "@mui/x-data-grid";
+import FlexBetween from "./FlexBetween";
+
+const DataGridCustomToolbar = () => {
+  return <div></div>;
+};
+
+export default DataGridCustomToolbar;
--- client/src/scenes/transactions/index.jsx
+++ client/src/scenes/transactions/index.jsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import { useGetTransactionsQuery } from "state/api";
 import Header from "components/Header";
-import { Box, useTheme } from "@mui/material";
+import { Box, Toolbar, useTheme } from "@mui/material";
+import { DataGridCustomToolbar } from "components/DataGridCustomToolbar";
 
 const Transactions = () => {
   const theme = useTheme();
@@ -51,7 +52,55 @@ const Transactions = () => {
     },
   ];
 
-  return <div>Transactions</div>;
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="Transactions" subtitle="Entire list of transactions" />
+      <Box
+        height="80vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-columnHeader": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: theme.palette.primary.light,
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderTop: "none",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${theme.palette.secondary[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          loading={isLoading || !data}
+          getRowId={(row) => row._id}
+          rows={(data && data.transactions) || []}
+          columns={columns}
+          rowCount={(data && data.total) || 0}
+          pagination
+          page={page}
+          pageSize={pageSize}
+          paginationMode="server"
+          sortingMode="server"
+          onPaginationModelChange={(newPage) => setPage(newPage)}
+          onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+          onSortModelChange={(newSortModel) => setSort(...newSortModel)}
+          components={{ Toolbar: DataGridCustomToolbar }}
+        />
+      </Box>
+    </Box>
+  );
 };
 
 export default Transactions;
