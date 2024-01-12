c3047c8e53a5888ca73ed6dd5f74aad460fc701e
Shehan Lakshitha <shehanlaksh@gmail.com>
2024-01-12 16:30:46 +0530

    Added daily chat

---
+++ client/src/scenes/daily/index.jsx
@@ -0,0 +1,52 @@
+import React, { useMemo, useState } from "react";
+import { Box, useTheme } from "@mui/material";
+import Header from "components/Header";
+import OverviewChart from "components/OverviewChart";
+import { useGetSalesQuery } from "state/api";
+import ReactDatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+const Daily = () => {
+  const [startDate, setStartDate] = useState(new Date("2021-02-01"));
+  const [endDate, setEndDate] = useState(new Date("2021-03-01"));
+  const { data } = useGetSalesQuery();
+  const theme = useTheme();
+
+  const [formattedData] = useMemo(() => {
+    if (!data) return [];
+
+    const { dailyData } = data;
+    const totalSalesLine = {
+      id: "totalSales",
+      color: theme.palette.secondary.main,
+      data: [],
+    };
+    const totalUnitsLine = {
+      id: "totalUnits",
+      color: theme.palette.secondary[600],
+      data: [],
+    };
+
+    Object.values(dailyData).forEach(({ date, totalSales, totalUnits }) => {
+      const dateFormatted = new Date(date);
+      if (dateFormatted >= startDate && dateFormatted <= endDate) {
+        const splitDate = date.substring(date.indexOf("-") + 1);
+
+        totalSalesLine.data = [
+          ...totalSalesLine.data,
+          { x: splitDate, y: totalSales },
+        ];
+        totalUnitsLine.data = [
+          ...totalUnitsLine.data,
+          { x: splitDate, y: totalUnits },
+        ];
+      }
+    });
+
+    return [[totalSalesLine], [totalUnitsLine]];
+  }, [data]); //eslint-disable-line react-hooks/exhaustive-deps
+
+  return <div>Daily</div>;
+};
+
+export default Daily;
