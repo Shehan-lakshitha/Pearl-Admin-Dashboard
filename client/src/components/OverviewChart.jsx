abc65b7f98f618dc5407d7e2a377fd01793694e8
Shehan Lakshitha <shehanlaksh@gmail.com>
2024-01-12 08:37:25 +0530

    Update OverviewChart.jsx

--- client/src/components/OverviewChart.jsx
+++ client/src/components/OverviewChart.jsx
@@ -3,7 +3,9 @@ import { ResponsiveLine } from "@nivo/line";
 import { useTheme } from "@mui/material";
 import { useGetSalesQuery } from "state/api";
 
-const OverviewChart = () => {
+const OverviewChart = ({ isDashboard = false, view }) => {
+  const theme = useTheme();
+  const { data, isLoading } = useGetSalesQuery();
   return <div>Overview</div>;
 };
 
