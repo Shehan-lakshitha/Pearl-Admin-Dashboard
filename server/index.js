f6f6a15ceafffa5d78dbdeaf06067b5dc403eb75
Shehan Lakshitha <shehanlaksh@gmail.com>
2024-01-17 21:49:29 +0530

    Add AffiliateStat model and insert data

--- server/index.js
+++ server/index.js
@@ -16,12 +16,14 @@ import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
 import OverallStat from "./models/OverallStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
 import {
   dataUser,
   dataProduct,
   dataProductStat,
   dataTransaction,
   dataOverallStat,
+  dataAffiliateStat,
 } from "./data/index.js";
 
 /* Config */
@@ -52,6 +54,7 @@ mongoose
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 
     /* Add data one time */
+    AffiliateStat.insertMany(dataAffiliateStat);
     //OverallStat.insertMany(dataOverallStat);
     // Product.insertMany(dataProduct);
     //ProductStat.insertMany(dataProductStat);
